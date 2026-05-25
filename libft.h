9801ab3341c569b324e0c5e60378fef9858b5ff1
lbalderr <lbalderr@student.42sp.org.br>
2026-05-25 14:19:58 -0300

    Add ft_tolower

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: lbalderr <lbalderr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/22 23:17:26 by lbalderr          #+#    #+#             */
-/*   Updated: 2026/05/25 13:56:43 by lbalderr         ###   ########.fr       */
+/*   Updated: 2026/05/25 14:19:25 by lbalderr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,5 +28,6 @@ void	*ft_memmove(void *dest, const void *src, size_t n);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
 int		ft_toupper(int c);
+int		ft_tolower(int c);
 
 #endif
