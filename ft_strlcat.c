f751ac829e22c69347d4d01a35247b604f684f33
lbalderr <lbalderr@student.42sp.org.br>
2026-05-25 13:52:49 -0300

    Add guard for size = 0

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: lbalderr <lbalderr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/25 12:57:04 by lbalderr          #+#    #+#             */
-/*   Updated: 2026/05/25 13:08:05 by lbalderr         ###   ########.fr       */
+/*   Updated: 2026/05/25 13:46:46 by lbalderr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,19 @@
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
 	size_t	i;
-	size_t	to_fill;
 	size_t	dst_len;
 	size_t	src_len;
 
 	src_len = ft_strlen(src);
+	if (size == 0)
+		return (src_len);
 	dst_len = 0;
 	while (dst[dst_len] && dst_len < size)
 		dst_len++;
 	if (size == dst_len)
 		return (size + src_len);
-	to_fill = size - dst_len - 1;
 	i = 0;
-	while (src[i] && i < to_fill)
+	while (src[i] && (dst_len + i + 1) < size)
 	{
 		dst[dst_len + i] = src[i];
 		i++;
