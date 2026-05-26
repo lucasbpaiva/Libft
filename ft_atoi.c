ced18d637ecb18276c3e2f14e7961b779a8d8ad7
lbalderr <lbalderr@student.42sp.org.br>
2026-05-26 11:51:51 -0300

    Add ft_atoi

---
+++ ft_atoi.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lbalderr <lbalderr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/26 10:47:26 by lbalderr          #+#    #+#             */
+/*   Updated: 2026/05/26 11:47:55 by lbalderr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int	ft_isspace(int c)
+{
+	return (c == ' ' || (c >= 9 && c <= 13));
+}
+
+int	ft_atoi(const char *nptr)
+{
+	int		i;
+	int		sign;
+	long	result;
+
+	i = 0;
+	sign = 1;
+	result = 0;
+	while (ft_isspace(nptr[i]))
+		i++;
+	if (nptr[i] == '+' || nptr[i] == '-')
+	{
+		if (nptr[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (ft_isdigit(nptr[i]))
+	{
+		if (result > (__LONG_MAX__ - (nptr[i] - '0')) / 10)
+			return ((sign == 1) * -1);
+		result = (result * 10) + (nptr[i] - '0');
+		i++;
+	}
+	return ((int)sign * result);
+}
