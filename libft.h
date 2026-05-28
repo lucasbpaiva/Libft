641f1ba0d037eb74bf56a156f1c334da0be3cf91
Lucas Paiva <lpaiva234@gmail.com>
2026-05-27 21:03:29 -0300

    Add ft_putchar_fd

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: lbalderr <lbalderr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/22 23:17:26 by lbalderr          #+#    #+#             */
-/*   Updated: 2026/05/27 20:45:59 by lbalderr         ###   ########.fr       */
+/*   Updated: 2026/05/27 20:57:08 by lbalderr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,5 +45,6 @@ char	**ft_split(char const *s, char c);
 char	*ft_itoa(int n);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void	ft_striteri(char *s, void (*f)(unsigned int, char*));
+void	ft_putchar_fd(char c, int fd);
 
 #endif
