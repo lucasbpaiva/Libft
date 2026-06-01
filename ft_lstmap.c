6313372a87b4bfcef4b92cc2047691839c240508
lbalderr <lbalderr@student.42sp.org.br>
2026-06-01 11:40:58 -0300

    Fix memory leak

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,11 +6,19 @@
 /*   By: lbalderr <lbalderr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/28 17:26:02 by lbalderr          #+#    #+#             */
-/*   Updated: 2026/05/28 21:01:31 by lbalderr         ###   ########.fr       */
+/*   Updated: 2026/06/01 11:07:05 by lbalderr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+static void	*ft_free_all(t_list **lst, void (*del)(void *), void *content)
+{
+	if (content)
+		del(content);
+	ft_lstclear(lst, del);
+	return (NULL);
+}
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
@@ -24,13 +32,11 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	while (lst)
 	{
 		new_content = f(lst->content);
+		if (!new_content)
+			return (ft_free_all(&mapped_list, del, NULL));
 		mapped_node = ft_lstnew(new_content);
 		if (!mapped_node)
-		{
-			del(new_content);
-			ft_lstclear(&mapped_list, del);
-			return (NULL);
-		}
+			return (ft_free_all(&mapped_list, del, new_content));
 		ft_lstadd_back(&mapped_list, mapped_node);
 		lst = lst->next;
 	}
